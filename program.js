894f30653657ddc582a44f91ada3294a39466246
fukayatsu <fukayatsu@gmail.com>
2015-08-18 17:20:56 +0900

    8

--- program.js
+++ program.js
@@ -1,4 +1,28 @@
-console.log({
-  [+process.argv[2] % 2 === 0 ? "even" : "odd"]: +process.argv[2],
-  [+process.argv[3] + +process.argv[2]]: +process.argv[3] + +process.argv[2]
-});
+const max = process.argv[2];
+let FizzBuzz = {
+  [Symbol.iterator]() {
+    let currentValue = 1;
+    return {
+      next() {
+        if (currentValue > max) return { done: true };
+        let value = null
+        if (currentValue % 15 == 0) {
+          value = 'FizzBuzz'
+        } else if (currentValue % 3 == 0) {
+          value = 'Fizz'
+        } else if (currentValue % 5 == 0) {
+          value = 'Buzz'
+        } else {
+          value = currentValue
+        }
+        currentValue++;
+
+        return { done: false, value: value }
+      }
+    }
+  }
+}
+
+for (var n of FizzBuzz) {
+  console.log(n);
+}
