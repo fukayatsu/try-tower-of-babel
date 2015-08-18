ad006e5e7e2e094a4d50991f2753cf4930b1fc29
fukayatsu <fukayatsu@gmail.com>
2015-08-18 17:30:23 +0900

    9

--- program.js
+++ program.js
@@ -1,27 +1,19 @@
 const max = process.argv[2];
-let FizzBuzz = {
-  [Symbol.iterator]() {
-    let currentValue = 1;
-    return {
-      next() {
-        if (currentValue > max) return { done: true };
-        let value = null
-        if (currentValue % 15 == 0) {
-          value = 'FizzBuzz'
-        } else if (currentValue % 3 == 0) {
-          value = 'Fizz'
-        } else if (currentValue % 5 == 0) {
-          value = 'Buzz'
-        } else {
-          value = currentValue
-        }
-        currentValue++;
-
-        return { done: false, value: value }
-      }
+let FizzBuzz = function*() {
+  let currentValue = 1;
+  while (currentValue <= max) {
+    let value = currentValue
+    if (currentValue % 15 == 0) {
+      value = 'FizzBuzz'
+    } else if (currentValue % 3 == 0) {
+      value = 'Fizz'
+    } else if (currentValue % 5 == 0) {
+      value = 'Buzz'
     }
+    currentValue++;
+    yield value;
   }
-}
+}();
 
 for (var n of FizzBuzz) {
   console.log(n);
