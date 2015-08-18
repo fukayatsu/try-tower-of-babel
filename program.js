debe9adb4608eff81427d56f808d839fc7ad91e6
fukayatsu <fukayatsu@gmail.com>
2015-08-18 17:55:15 +0900

    12

--- program.js
+++ program.js
@@ -1,4 +1,11 @@
-var inputs = process.argv.slice(2);
-var result = inputs.map(word => word[0])
-                   .reduce((result, current) => result + current);
-console.log(result);
+var args = process.argv[2].split(",");
+args = args.map((arg)=> +arg);
+
+// ここに平均を取るavg関数を作る、作る際には
+// RESTパラメータを利用すること。
+function avg(...args) {
+  let sum = args.reduce( (sum, n) => sum + n );
+  return sum / args.length
+}
+
+console.log(avg(...args));
